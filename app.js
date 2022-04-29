9bd14e0fdd8468675c68025d89f5e1ecaefe319f
Mitesh Tank <codewithmitesh@gmail.com>
2022-04-29 17:13:00 +0530

    Added Comments in Apppjs

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cookieParser = require('cookie-parser')
+// For Body parser 
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override')
 const app = express();
-
+// authentication 
 const auth = require('./middlewares/auth')
 
-
+// Routers 
 const landingRouter = require('./routes/landingRoute')
 const clubRouter = require('./routes/clubRoute');
 const adminRouter = require('./routes/adminRoute')
@@ -21,7 +22,7 @@ app.set("view engine", "pug")
 app.use(express.static('public'))
 app.use(cookieParser())
 app.use(methodOverride('_method'))
-
+// checking for Authentication Status and redirecting to pages
 app.use('/', landingRouter)
 app.use('/clubs', auth.isAuthenticatd, clubRouter);
 app.use('/admin', auth.isAuthenticatd, auth.isFaculty, adminRouter)
